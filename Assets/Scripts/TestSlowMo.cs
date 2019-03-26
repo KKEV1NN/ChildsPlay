2ce83b4831a7d70f0f2a81efb52b205ef1b0077f
Gordon <aardappel156@gmail.com>
2019-03-26 16:09:14 +0100

    Created sliders And added cooldown
    The timer of the cooldown need some tweaking but it is working

--- Assets/Scripts/TestSlowMo.cs
+++ Assets/Scripts/TestSlowMo.cs
@@ -7,12 +7,14 @@ public class TestSlowMo : MonoBehaviour
 {
     public Slider slowMoBar;
     public bool slowMoStart;
+    public bool slowMoCool;
+
 
     // Start is called before the first frame update
     void Start()
     {
         slowMoBar = GameObject.FindObjectOfType<Slider>();
-        slowMoStart = true;
+        //slowMoStart = true;
 
 
     }
@@ -20,42 +22,52 @@ public class TestSlowMo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.V))
+        if (!slowMoCool && Input.GetKey(KeyCode.V))
         {
             SlowMo(false);
             slowMoStart = true;
-        }
-        else
-        {
-            SlowMo(true);
-            slowMoStart = false;
-        }
-
-        if(slowMoBar.value < 0)
-        {
-            SlowMo(false);
-        }
-
-    }
-
-    void SlowMo(bool b)
-    {
-        if (b)
-        {
-            Time.timeScale = 0.25f;
-            Time.fixedDeltaTime = 0.02f * Time.timeScale;
 
             slowMoBar.value -= Time.unscaledDeltaTime;
         }
         else
         {
+            SlowMo(true);
+            slowMoStart = false;
+
+            slowMoBar.value += Time.unscaledDeltaTime;
+        }
+
+        SlowMoCDW();
+    }
+
+    void SlowMo(bool b)
+    {
+
+        if (b)
+        {
             Time.timeScale = 1;
             Time.fixedDeltaTime = 0.02f;
 
-            slowMoBar.value += Time.unscaledDeltaTime;
-
-
-
         }
+        else
+        {
+            Time.timeScale = 0.25f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+    }
+
+    void SlowMoCDW()
+    {
+
+        if(slowMoBar.value == 0)
+        {
+            slowMoCool = true;
+            Invoke("ChangeBool", 3);
+        }
+    }
+
+    void ChangeBool()
+    {
+        slowMoCool = false;
     }
 }
